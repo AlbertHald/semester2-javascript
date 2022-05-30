206144a5f244d2ae3a324fb419f3557f20c50af5
AlbertHald <albert24122@gmail.com>
2022-05-30 13:56:59 +0200

    Sejt commit

---
+++ async js/spy-cite/node/server.js
@@ -0,0 +1,56 @@
+import http from 'http';
+import fs from 'fs';
+
+const hostname= '127.0.0.1'
+const port = '8080'
+
+const server = http.createServer(requestHandle);
+
+function requestHandle(req, res) {
+    try {
+        /* res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.readFile('resources/cite.html', (error, data)=> {
+            if (error) {
+                res.writeHead(404);
+                res.write('Error: Page not found');
+            } else {
+                res.write(data);    
+            }
+            res.end();
+        }) */
+        
+        processReq(req,res);
+    } catch (error) {
+        console.log("internal Error: " + error);
+        //errorRespone(res,500,"");
+    }
+};
+
+//Handles the requests.
+function processReq(req,res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('resources/cite.html', (error, data)=> {
+        if (error) {
+            //Send status 404 (Not found)
+            res.writeHead(404);
+            res.write('Error: File not found');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    });
+}
+
+/* function errorRespone(res, code, reason) {
+    res.statuscode=code;
+    res.setHeader('Content-Type', 'text/txt');
+    res.write(reason);
+} */
+
+server.listen(port, (error) => {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log(`Server is listening on port: ` + port);
+    }
+});
